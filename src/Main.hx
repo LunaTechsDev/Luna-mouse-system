78d490b19af6749e634a090e0b3025b64e7fb259
inc0der <inc0der.code@gmail.com>
2020-09-21 21:38:37 -0300

    v0.1.0 (#1)
    * Add TouchInput mouse move listening methods
    
    * Update to compile to correct game folders
    
    * Add cursor and a listener to stage
    
    * Add Scene_Base methods to remove mouse listener and ensure sprite is on top
    
    * Add Main to apply patches
    
    * Update launch.json for debugging
    
    * Update checkstyle.json to remove method length rule
    
    * Add a publish.hxml for automation
    
    * Add scripts to package.json for GitHub actions
    
    * Add GitHub action for automation
    
    * Add Parameters.js to compilation
    
    * Update GitHub actions
    
    * Fix missing jobs key
    
    * Fix to only build plugins on new tag
    
    * Add a job to build a nightly version of plugin on each push to master
    
    * Change name of artifacts and jobs

--- src/Main.hx
+++ src/Main.hx
@@ -1,4 +1,11 @@
+import scenes.LunaSceneBase;
+import core.LunaTouchInput;
+import core.LunaStage;
+
 class Main {
   public static function main() {
+    LunaTouchInput.patch();
+    LunaStage.patch();
+    LunaSceneBase.patch();
   }
 }
