ba09c957d8043a60a5d141beec1c4ac460892698
inc0der <inc0der.code@gmail.com>
2020-09-23 19:30:24 -0300

    Add and register plugin commands

--- src/Main.hx
+++ src/Main.hx
@@ -1,14 +1,20 @@
+import rm.managers.ImageManager;
+import core.CursorLoader;
+import sprites.CursorSprite;
+import core.Amaryllis;
 import rm.Globals;
 import scenes.LunaSceneBase;
 import core.LunaTouchInput;
 import core.LunaStage;
 import utils.Parse;
+import rm.managers.PluginManager;
 import rm.types.RM.PluginSettings;
 
 using StringTools;
 using Lambda;
 
 class Main {
+  public static var pluginName: String = 'Luna_MouseSystem';
   public static var params: Dynamic;
 
   public static function main() {
@@ -17,5 +23,31 @@ class Main {
     LunaTouchInput.patch();
     LunaStage.patch();
     LunaSceneBase.patch();
+    registerPluginCommands();
   }
+
+  public static function registerPluginCommands () {
+    #if !compileMV
+    PluginManager.registerCommand(pluginName, 'showCursor', (args) -> {
+      var cursor: CursorSprite = untyped Amaryllis.currentScene()._cursor;
+      cursor.visible = true;
+      untyped document.body.style.cursor = 'none';
+    });
+    
+    PluginManager.registerCommand(pluginName, 'hideCursor', (args) -> {
+      var option: Dynamic = Parse.parseParameters(args);
+      var cursor: CursorSprite = untyped Amaryllis.currentScene()._cursor;
+      cursor.visible = false;
+      if (option.showDefault) {
+        untyped document.body.style.cursor = 'auto';
+      }
+    });
+
+    PluginManager.registerCommand(pluginName, 'changeCursor', (args: Dynamic) -> {
+      var bitmap = ImageManager.loadSystem(args.filename);
+      untyped Amaryllis.currentScene()._cursor.bitmap = bitmap;
+    });
+    #end
+  }
+
 }
