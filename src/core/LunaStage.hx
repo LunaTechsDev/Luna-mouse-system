a5ffcd1ddf724bd7c3f165801e7b669aa32bc9bd
inc0der <inc0der.code@gmail.com>
2020-09-24 11:36:55 -0300

    Add all cursors from parameters to CursorLoader

--- src/core/LunaStage.hx
+++ src/core/LunaStage.hx
@@ -1,12 +1,15 @@
 package core;
 
+import sprites.CursorSprite;
 import rm.core.TouchInput;
 import core.Types.JsFn;
 import utils.Comment;
+import types.CursorData;
 import rm.core.Stage;
 import macros.FnMacros.self as sf;
 
 using utils.Fn;
+using Lambda;
 
 class LunaStage {
   public static inline function patch() {
@@ -27,8 +30,20 @@ class LunaStage {
 
     Stage.setPrPropFn('createMouseCursor', () -> {
       sf(Stage, {
+        var cursors: Array<Dynamic> = Main.params.cursors;
+        for (cursor in cursors) {
+          CursorLoader.addCursor({
+            name: cursor.filename,
+            url: 'img/system/${cursor.filename}.png',
+          });
+        }
         untyped {
-          self._cursor = CursorLoader.createCursor();
+          var defaultCursor = Main.params.cursors[0];
+          var bitmap = ImageManager.loadSystem(defaultCursor.filename);
+          self._cursor = new CursorSprite(bitmap, {
+            name: defaultCursor.filename,
+            url: bitmap.url
+          });
           self.addChild(self._cursor);
         }
       });
