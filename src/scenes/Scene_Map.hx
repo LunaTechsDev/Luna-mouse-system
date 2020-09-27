41039e3dc8d7a835fbcf7ed173d39a7ed297a6d8
inc0der <inc0der.code@gmail.com>
2020-09-27 11:05:50 -0300

    Add cursor swap on event touch

--- src/scenes/Scene_Map.hx
+++ src/scenes/Scene_Map.hx
@@ -3,10 +3,12 @@ package scenes;
 import rm.types.RPG.Event;
 import rm.Globals.DataMap;
 import rm.Globals.GameMap;
+import core.CursorLoader;
 import rm.objects.Game_Event;
 import rm.scenes.Scene_Map as RmScene_Map;
 
 using Lambda;
+using StringTools;
 
 class Scene_Map extends RmScene_Map {
   public function isEventTouched(event: Game_Event): Bool {
@@ -26,7 +28,10 @@ class Scene_Map extends RmScene_Map {
     if (eventTouched != null) {
       var eventData: Event = DataMap.events[eventTouched.eventId()];
       if (eventData.note != null) {
-        trace('Event touched with note ${eventData.note}');
+        var cursorName = eventData.note.split(':')[1];
+        if (cursorName != null) {
+          CursorLoader.changeCursor(cursorName.trim());
+        }
       }
     }
   }
