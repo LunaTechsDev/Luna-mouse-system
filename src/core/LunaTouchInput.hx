227c48ff0db4ae6c952a079f7b601b5b5ca9588e
inc0der <inc0der.code@gmail.com>
2020-09-26 23:52:42 -0300

    Refactor TouchInput to use new jsPatch macro

--- src/core/LunaTouchInput.hx
+++ src/core/LunaTouchInput.hx
@@ -1,68 +1,50 @@
 package core;
 
-import core.Types.JsFn;
-import utils.Comment;
+import js.html.MouseEvent;
 import rm.core.TouchInput;
-import macros.FnMacros.self as sf;
 
-using utils.Fn;
+using Lambda;
 
-class LunaTouchInput {
-  public static inline function patch() {
-    Comment.title('TouchInput');
+class LunaTouchInput extends TouchInput {
+  private var mouseX: Float;
+  private var mouseY: Float;
+  private var _mouseMoveListeners: Array<Dynamic>;
 
-    var oldInitialize: JsFn = TouchInput.initializeR;
-    TouchInput.initializeD = () -> {
-      sf(TouchInput, {
-        oldInitialize.call(self);
+  public function initialize() {
+    untyped _TouchInput_initialize.call(this);
+    this._mouseMoveListeners = [];
+    this.mouseX = 0;
+    this.mouseY = 0;
+  }
 
-        untyped {
-          self._mouseMoveListeners = [];
-          self.mouseX = 0;
-          self.mouseY = 0;
-        };
-      });
+  private function _onMouseMove(event: MouseEvent) {
+    untyped _TouchInput__onMouseMove.call(this, event);
+    if (this._mouseMoveListeners.length > 0) {
+      for (listener in this._mouseMoveListeners) {
+        listener(event);
+      }
+      this.mouseX = event.clientX;
+      this.mouseY = event.clientY;
     }
+  }
 
-    var oldOnMouseMove: JsFn = untyped TouchInput._onMouseMove;
-    untyped TouchInput._onMouseMove = (event) -> {
-      sf(TouchInput, {
-        oldOnMouseMove.call(self, event);
-        untyped {
-          if (self._mouseMoveListeners.length > 0) {
-            var listeners: Array<Dynamic> = self._mouseMoveListeners;
-            for (listener in listeners) {
-              listener(event);
-            }
-          }
-          self.mouseX = event.clientX;
-          self.mouseY = event.clientY;
-        }
-      });
+  public function addMouseMoveListener(listener: Any) {
+    if (listener != null) {
+      this._mouseMoveListeners.push(listener);
+    } else {
+      throw new js.lib.Error('Cursor move event listener must be of type function');
     }
+  }
 
-    Fn.setField(TouchInput, 'addMouseMoveListener', (func: Dynamic, context: Dynamic) -> {
-      sf(TouchInput, {
-        if (func != null) {
-          untyped self._mouseMoveListeners.push(func);
-        } else {
-          throw new js.lib.Error('Cursor move event listener must be of type function');
-        }
-      });
-    });
-
-    Fn.setField(TouchInput, 'removeMouseMoveListener', (func: Dynamic) -> {
-      sf(TouchInput, {
-        if (func != null) {
-          var listenerToRemove = untyped self._mouseMoveListeners.find(listener -> listener == func);
-          if (listenerToRemove) {
-            var index = untyped self._mouseMoveListeners.indexOf(listenerToRemove);
-            untyped self._mouseMoveListeners.splice(index, 1);
-          }
-        } else {
-          throw new js.lib.Error('Cursor move event listener must be of type function');
-        }
-      });
-    });
+  public function removeMouseMoveListener(listener: Any) {
+    if (listener != null) {
+      var listenerToRemove = this._mouseMoveListeners.find(l -> l == listener);
+      if (listenerToRemove) {
+        var index = untyped this._mouseMoveListeners.indexOf(listenerToRemove);
+        untyped this._mouseMoveListeners.splice(index, 1);
+      }
+    } else {
+      throw new js.lib.Error('Cursor move event listener must be of type function');
+    }
   }
 }
