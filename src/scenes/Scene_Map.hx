166672b2407e47001ec75eb3745a35166683143d
inc0der <inc0der.code@gmail.com>
2020-09-27 10:29:03 -0300

    Add Scene_Map with basic event touch functionality

---
+++ src/scenes/Scene_Map.hx
@@ -0,0 +1,33 @@
+package scenes;
+
+import rm.types.RPG.Event;
+import rm.Globals.DataMap;
+import rm.Globals.GameMap;
+import rm.objects.Game_Event;
+import rm.scenes.Scene_Map as RmScene_Map;
+
+using Lambda;
+
+class Scene_Map extends RmScene_Map {
+  public function isEventTouched(event: Game_Event): Bool {
+    var mouseMapX = GameMap.canvasToMapX(untyped TouchInput.mouseX);
+    var mouseMapY = GameMap.canvasToMapY(untyped TouchInput.mouseY);
+    return event.x == mouseMapX && event.y == mouseMapY;
+  }
+
+  public function eventTouchedByMouse(): Game_Event {
+    var events: Array<Game_Event> = GameMap.events();
+    return events.find(event -> isEventTouched(event));
+  }
+
+  public override function updateMain() {
+    untyped _Scene_Map_updateMain.call(this);
+    var eventTouched = eventTouchedByMouse();
+    if (eventTouched != null) {
+      var eventData: Event = DataMap.events[eventTouched.eventId()];
+      if (eventData.note != null) {
+        trace('Event touched with note ${eventData.note}');
+      }
+    }
+  }
+}
